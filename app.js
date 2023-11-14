080c28d94d38a42d2cce048eefe8790dbc340bb9
mathiasburger <mburakyilmazer38@gmail.com>
2023-11-15 02:25:50 +0300

    grupp

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-fetch("data.json").then(res=>res.json()).then(data=>console.log(data))
+fetch("data.json")
+  .then((res) => res.json())
+  .then((data) => showBlok(data));
+
+const showBlok = (data) => {
+  const days = document.querySelector(".days");
+  data.forEach((item) => {
+    days.innerHTML += `<div class="grup">
+    <div style="height: ${3*item.amount}px">
+        </div>
+        <p>${item.day}</p>
+        </div>`;
+  });
+};
