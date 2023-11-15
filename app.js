7fb3be81ff4247bdb9a0d03aa779f03bb4d1ce2a
mathiasburger <mburakyilmazer38@gmail.com>
2023-11-15 03:40:36 +0300

    last commit

--- app.js
+++ app.js
@@ -5,10 +5,21 @@ fetch("data.json")
 const showBlok = (data) => {
   const days = document.querySelector(".days");
   data.forEach((item) => {
-    days.innerHTML += `<div class="grup">
-    <div style="height: ${3*item.amount}px">
-        </div>
-        <p>${item.day}</p>
-        </div>`;
+    days.innerHTML += `
+    <div class="grup">
+  <div class="amount" style="height: ${2.5 * item.amount}px">
+    <div class="go">$${item.amount}</div>
+  </div>
+  <p>${item.day}</p>
+</div>`;
   });
+  let arr = [];
+  data.map((item) => {
+    arr.push(item.amount);
+  });
+  let index = arr.indexOf(Math.max(...arr));
+  console.log(index);
+  document.querySelector(
+    `.days .grup:nth-child(${index + 1}) .amount`
+  ).style.backgroundColor = "hsl(186, 34%, 60%)";
 };
